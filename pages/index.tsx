fc2ee837542240ab4fd4953ed28b060872fccf2a
João P Porto <jpportodev@gmail.com>
2022-12-12 15:01:09 -0300

    commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,7 @@ import Questionario from '../components/Questionario'
 import QuestaoModel from '../model/questao'
 import { useRouter } from 'next/router'
 
-const BASE_URL = 'https://quiz-kutu-jpportodev.vercel.app/api'
+const BASE_URL = process.env.NEXT_PUBLIC_API
 
 export default function Home() {
   const router = useRouter()
