fd282c5e29ab4318785c74ed94351625b67148b1
João P Porto <jpportodev@gmail.com>
2022-12-12 15:25:06 -0300

    commit

--- components/Questionario.tsx
+++ components/Questionario.tsx
@@ -23,7 +23,7 @@ export default function Questionario(props: QuestionarioProps) {
             {props.questao ?
                 <Questao
                     valor={props.questao}
-                    tempoPraResposta={6}
+                    tempoPraResposta={20}
                     respostaFornecida={respostaFornecida}
                     tempoEsgotado={props.irPraProximoPasso} />
                 : false
